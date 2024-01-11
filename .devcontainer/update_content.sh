e7b7d76b94afd16fd4a1eef318bac45a7d2aaf64
Mathias Kende <mathias@kende.fr>
2024-01-12 00:02:51 +0100

    Add a GitHub devcontainer configuration.

---
+++ .devcontainer/update_content.sh
@@ -0,0 +1,9 @@
+# DO NOT EDIT! This file is written by perl_setup_dist.
+# If needed, you can add content at the end of the file.
+# This script is executed during the updateContent phase of the container lifecycle.
+# It contains all the commands that need access to the repository content and so,
+# cannot be executed as part of the Dockerfile.
+
+sudo cpanm --notest --installdeps --with-develop --with-configure --with-recommends --with-suggests --with-all-features .
+perl Makefile.PL
+# End of the template. You can add custom content below this line.
