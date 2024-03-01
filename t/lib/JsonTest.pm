30526f95680197a831bbe1a20d92f8b25b860de2
Mathias Kende <mathias@kende.fr>
2024-03-01 22:57:36 +0100

    Improve the test suite by running it in Perl rather than through the cmark python tool.

---
+++ t/lib/JsonTest.pm
@@ -0,0 +1,31 @@
+package JsonTest;
+
+use strict;
+use warnings;
+use utf8;
+use feature ':5.24';
+
+use Exporter 'import';
+use JSON 'from_json';
+use Test2::V0;
+
+our @EXPORT = qw(test_suite);
+
+sub test_suite {
+  my ($json_file, $pmarkdown, %opt) = @_;
+  my $test_data;
+  {
+    local $/ = undef;
+    open my $f, '<:encoding(utf-8)', $json_file;
+    my $json_data = <$f>;
+    close $f;
+    $test_data = from_json($json_data);
+  }
+  for my $t (@{$test_data}) {
+    my $title = sprintf "%s (%d)", $t->{section}, $t->{example};
+    my @diag;
+    push @diag, sprintf $opt{test_url}, $t->{example} if exists $opt{test_url};
+    push @diag, 'Input markdown:', $t->{markdown}, "\n";
+    is($pmarkdown->convert($t->{markdown}), $t->{html}, $title, @diag);
+  }
+}
